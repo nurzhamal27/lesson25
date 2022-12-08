75aeaf4fa5bcf211c1588ab5133f4548fdaa7a4d
nurzhamal27 <119873898+nurzhamal27@users.noreply.github.com>
2022-12-08 17:13:00 +0600

    Delete Variant.java

--- Variant.java
+++
@@ -1,8 +0,0 @@
-package RockPaperScissorsLizardSpock;
-
-import javax.swing.plaf.PanelUI;
-
-public class Variant {
-    public enum CHOICE{ROCK, PAPER, SCISSORS};
-    public enum RESULT{WIN, LOSE, TIE};
-}
